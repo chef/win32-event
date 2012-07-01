4e9eb176b1822dd44577d67bcbe82f4a9650f913
Daniel Berger <djberg96@gmail.com>
2012-07-01 17:44:52 -0600

    Start conversion to FFI.

--- lib/win32/event.rb
+++ lib/win32/event.rb
@@ -5,31 +5,33 @@ module Win32
 
   # The Event class encapsulates Windows event objects.
   class Event < Ipc
-   
+    ffi_lib :kernel32
+
+    attach_function :CreateEvent, :CreateEventW, [:pointer, :bool, :bool, :buffer_in], :ulong
+    attach_function :OpenEvent, :OpenEventW, [:ulong, :bool, :buffer_in], :ulong
+    attach_function :SetEvent, [:ulong], :bool
+    attach_function :ResetEvent, [:ulong], :bool
+
     # This is the error raised if any of the Event methods fail.
     class Error < StandardError; end
-      
-    extend Windows::Synchronize
-    extend Windows::Error
-    extend Windows::Handle
-      
+
     # The version of the win32-event library
-    VERSION = '0.5.2'
-   
-    # The name of the Event object.
+    VERSION = '0.6.0'
+
+    # The name of the Event object. The default is nil
     #
     attr_reader :name
-      
+
     # Indicates whether or not the Event requires use of the ResetEvent()
-    # function set the state to nonsignaled.
+    # function set the state to nonsignaled. The default is false
     #
     attr_reader :manual_reset
-      
+
     # The initial state of the Event object. If true, the initial state
-    # is signaled. Otherwise, it is non-signaled.
+    # is signaled. Otherwise, it is non-signaled. The default is false.
     #
     attr_reader :initial_state
-      
+
     # Creates and returns new Event object.  If +name+ is omitted, the
     # Event object is created without a name, i.e. it's anonymous.
     #
@@ -48,19 +50,19 @@ module Win32
     #
     # If the +inherit+ parameter is true, then processes created by this
     # process will inherit the handle. Otherwise they will not.
-    # 
+    #
     # In block form this will automatically close the Event object at the
     # end of the block.
-    # 
+    #
     def initialize(name=nil, man_reset=false, init_state=false, inherit=true)
       @name          = name
       @manual_reset  = man_reset
       @initial_state = init_state
       @inherit       = inherit
-         
+
       manual_reset  = man_reset ? 1 : 0
       initial_state = init_state ? 1 : 0
-         
+
       # Used to prevent potential segfaults.
       if name && !name.is_a?(String)
         raise TypeError, 'name must be a string'
@@ -73,15 +75,15 @@ module Win32
       else
         sec = 0
       end
-         
+
       handle = CreateEvent(sec, manual_reset, initial_state, name)
-         
+
       if handle == 0 || handle == INVALID_HANDLE_VALUE
         raise Error, get_last_error
       end
- 
+
       super(handle)
-         
+
       if block_given?
         begin
           yield self
@@ -90,7 +92,7 @@ module Win32
         end
       end
     end
-      
+
     # Open an existing Event by +name+. The +inherit+ argument sets whether
     # or not the object was opened such that a process created by the
     # CreateProcess() function (a Windows API function) can inherit the
@@ -107,7 +109,7 @@ module Win32
       if name && !name.is_a?(String)
         raise TypeError, 'name must be a string'
       end
-         
+
       bool = inherit ? 1 : 0
 
       # This block of code is here strictly to force an error if the user
@@ -124,7 +126,7 @@ module Win32
 
       self.new(name, false, false, inherit, &block)
     end
-      
+
     # Returns whether or not the object was opened such that a process
     # created by the CreateProcess() function (a Windows API function) can
     # inherit the handle. The default is true.
@@ -132,34 +134,34 @@ module Win32
     def inheritable?
       @inherit
     end
-      
+
     # Sets the Event object to a non-signaled state.
-    # 
+    #
     def reset
       unless ResetEvent(@handle)
         raise Error, get_last_error
       end
       @signaled = false
     end
-      
+
     # Sets the Event object to a signaled state.
-    # 
+    #
     def set
       unless SetEvent(@handle)
         raise Error, get_last_error
       end
       @signaled = true
     end
-      
+
     # Synonym for Event#reset if +bool+ is false, or Event#set
     # if +bool+ is true.
-    # 
+    #
     def signaled=(bool)
       if bool
         set
       else
         reset
       end
-    end 
+    end
   end
 end
