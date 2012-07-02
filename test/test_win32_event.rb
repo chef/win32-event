595d877c02a9075467078aee6b567df682d914be
Daniel Berger <djberg96@gmail.com>
2012-07-01 20:43:47 -0600

    More test cleanup and refactoring.

--- test/test_win32_event.rb
+++ test/test_win32_event.rb
@@ -17,7 +17,7 @@ class TC_Win32Event < Test::Unit::TestCase
     @ascii   = "foo"
   end
 
-  def test_version
+  test "version is set to expected value" do
     assert_equal('0.6.0', Event::VERSION)
   end
 
@@ -34,54 +34,88 @@ class TC_Win32Event < Test::Unit::TestCase
     assert_nothing_raised{ @event = Event.new(@unicode) }
   end
 
-=begin
-  def test_constructor_errors
-    assert_raises(ArgumentError){ Event.new("Foo", true, false, true, 1) }
-    assert_raises(TypeError){ Event.new(1) }
+  test "constructor accepts a maximum of four arguments" do
+    assert_raises(ArgumentError){ Event.new('Foo', true, false, true, 1) }
   end
 
-  def test_open
+  test "first argument to constructor must be a string" do
+    assert_raise(TypeError){ Event.new(1) }
+  end
+
+  test "open method basic functionality" do
+    event = Event.new(@ascii)
     assert_respond_to(Event, :open)
-    assert_nothing_raised{ Event.open("Bar"){} }
-    assert_nothing_raised{ Event.open("Ηελλας"){} }
-    assert_nothing_raised{ Event.open("Bar", false) }
+    assert_nothing_raised{ @event = Event.open(@ascii) }
+    event.close
   end
 
-  def test_open_expected_errors
-    assert_raises(ArgumentError){ Event.open("Bar", true, false){} }
-    assert_raises(Event::Error){ Event.open("Blah"){} }
+  test "open method accepts an inherit argument" do
+    event = Event.new(@ascii)
+    assert_nothing_raised{ @event = Event.open(@ascii, false) }
+    assert_false(@event.inheritable?)
+    event.close
+  end
+
+  test "open method works with unicode names" do
+    event = Event.new(@unicode)
+    assert_nothing_raised{ @event = Event.open(@unicode) }
+    event.close
+  end
+
+  test "open method requires a name" do
+    assert_raise(ArgumentError){ Event.open }
+  end
+
+  test "open method requires a string argument as the first argument" do
     assert_raises(TypeError){ Event.open(1){} }
   end
 
-  def test_inheritable
-    @event1 = Event.open("Foo")
-    @event2 = Event.open("Baz", false)
-
-    assert_respond_to(@event1, :inheritable?)
-    assert_nothing_raised{ @event1.inheritable? }
-    assert_true(@event1.inheritable?)
-    assert_false(@event2.inheritable?)
+  test "open accepts a maximum of two arguments" do
+    assert_raise(ArgumentError){ Event.open('bogus', true, false) }
   end
 
-  def test_name
-    assert_respond_to(@event1, :name)
-    assert_nothing_raised{ @event1.name }
-    assert_nothing_raised{ @uni_event.name }
-    assert_nil(@event1.name)
-    assert_kind_of(String, @event2.name)
+  test "open method fails if name cannot be found" do
+    assert_raise(Errno::ENOENT){ Event.open('bogus') }
   end
 
-  def test_initial_state
-    assert_respond_to(@event1, :initial_state)
-    assert_nothing_raised{ @event1.initial_state }
-
-    assert_false(@event1.initial_state)
-    assert_false(@event2.initial_state)
-    assert_false(@event3.initial_state)
-    assert_true(@event4.initial_state)
+  test "inheritable basic functionality" do
+    assert_respond_to(@event, :inheritable?)
+    assert_nothing_raised{ @event.inheritable? }
   end
 
-=end
+  test "inheritable is true by default" do
+    assert_true(@event.inheritable?)
+  end
+
+  test "inheritable matches value passed to constructor" do
+    @event = Event.new(nil, false, false, false)
+    assert_false(@event.inheritable?)
+  end
+
+  test "name attribute basic functionality" do
+    assert_respond_to(@event, :name)
+    assert_nothing_raised{ @event.name }
+  end
+
+  test "name is nil by default" do
+    assert_nil(@event.name)
+  end
+
+  test "initial_state basic functionality" do
+    assert_respond_to(@event, :initial_state)
+    assert_nothing_raised{ @event.initial_state }
+    assert_boolean(@event.initial_state)
+  end
+
+  test "initial_state is false by default" do
+    assert_false(@event.initial_state)
+  end
+
+  test "initial state matches value set in constructor" do
+    @event = Event.new(nil, false, true)
+    assert_true(@event.initial_state)
+  end
+
   test "manual_reset basic functionality" do
     assert_respond_to(@event, :manual_reset)
     assert_nothing_raised{ @event.manual_reset }
